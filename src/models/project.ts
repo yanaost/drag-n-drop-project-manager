56d1bbc959d20a73545a037655d431a785aa1c88
Yana Os <yanaos@Yanas-MacBook-Pro-2.local>
2024-01-17 16:04:53 +1100

    refactoring and reorganisation based on ES Modules

---
+++ src/models/project.ts
@@ -0,0 +1,14 @@
+export class Project {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: ProjectStatus
+    ) {}
+}
+
+export enum ProjectStatus {
+    Active,
+    Finished,
+}
